220cdf9ead0a10c2d559f9bd3df579e1b926e3ca
Nicolas Pogulanik <npogulanik@gmail.com>
2017-07-25 15:18:57 -0300

    Gateway Created

---
+++ blog/src/main/java/org/npogulanik/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package org.npogulanik.config.audit;
---
+++ blog/src/main/java/org/npogulanik/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package org.npogulanik.repository.search;
+
+import org.npogulanik.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
